28869a78f852602dd016322803c3825d83a5063e
katmart93 <katwlodarczyk93@gmail.com>
2022-08-12 15:31:28 +0200

    states created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,11 @@ import Container from "./reusableComponents/Container";
 import CardContainer from "./CardContainer";
 
 export default function Hero() {
+  const [totalPrice, setTotallPrice] = useState(0);
+  const [itemCount, setItemCount] = useState(0);
+  const [cartItems, setCartItems] = useState([]);
+  const [hiddenMenu, setHiddenMenu] = useState(false);
+
   return (
     <section className="hero">
       <Nav />
