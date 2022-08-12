e29cdc81a2f4da51853a85007751ac22d90a97ec
katmart93 <katwlodarczyk93@gmail.com>
2022-08-12 14:44:12 +0200

    hero component updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
+// components
+import Nav from "./Nav";
+import Container from "./reusableComponents/Container";
+
 export default function Hero() {
-  return <section className="hero"></section>;
+  return (
+    <section className="hero">
+      <Nav />
+      <Container></Container>
+    </section>
+  );
 }
