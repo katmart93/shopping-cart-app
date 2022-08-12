8f07a22349c2a03ba2c8e9eeb2d8ededdd7cd7b4
katmart93 <katwlodarczyk93@gmail.com>
2022-08-12 12:22:09 +0200

    shedata componenet created

---
+++ src/components/ShoeData.jsx
@@ -0,0 +1,34 @@
+// images
+import ShoeOne from "../img/shoe1.png";
+import ShoeTwo from "../img/shoe2.png";
+import ShoeThree from "../img/shoe3.png";
+import ShoeFour from "../img/shoe4.png";
+import ShoeFive from "../img/shoe5.png";
+import ShoeSix from "../img/shoe6.png";
+
+export default [
+  {
+    cardImg: ShoeOne,
+    price: 299,
+  },
+  {
+    cardImg: ShoeTwo,
+    price: 199,
+  },
+  {
+    cardImg: ShoeThree,
+    price: 399,
+  },
+  {
+    cardImg: ShoeFour,
+    price: 499,
+  },
+  {
+    cardImg: ShoeFive,
+    price: 599,
+  },
+  {
+    cardImg: ShoeSix,
+    price: 249,
+  },
+];
