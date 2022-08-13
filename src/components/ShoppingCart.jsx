f1be490929152f5efc360027346966ef0b4cbddd
katmart93 <katwlodarczyk93@gmail.com>
2022-08-13 14:20:13 +0200

    shoppingcart component updated

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function ShoppingCart({ cartItems }) {
-  return <div className="shoppingCart"></div>;
+  return (
+    <div className="shoppingCart">
+      {cartItems.map((item) => (
+        <div key={item.id} className="cardRow">
+          <div className="cardImg">
+            <p>x{item.cartCount}</p>
+            <img src={item.img} alt="shoe" />
+          </div>
+          <div className="cardPrice">
+            <p>${item.price}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
